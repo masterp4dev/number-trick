9dd6d3217256be9ccba4c8c609a7317960ba1a83
Jubril Issa <jubrilissa@gmail.com>
2015-06-12 11:04:09 +0100

    update the test file

--- test.py
+++ test.py
@@ -3,22 +3,22 @@ import time
 begin_time = time.clock()
 
 
-def numbers(lsnum, numN, numM):
-	for s in lsnum:
-		myList = []
-    	for i in range(1, n+1):
-        	if n % i == 0:
-            	myList.append(i)
-        return mylist
 
-	result = []
+# fucntion to get the answers
+def numbers(lsnums, numN, numM):
+
+	for rst in lsnums:
+		# return i
+	    return common_factors(rst)
+	
 	# result > numN
-	return result
+	# return result
 
 
 # numbers(2, 3, 4)
 
-def commonfactors(n):
+# get the common factors
+def common_factors(n):
     myList = []
     for i in range(1, n+1):
         if n % i == 0:
@@ -27,11 +27,20 @@ def commonfactors(n):
     return myList
 
 
+
+# flatten the list 
+def flatten_list(lists):
+    results = []
+    for numbers in lists:
+        results.append(lst)
+        return results   
+
+
 first_list = [9, 4, 36, 1]
 
 
-print numbers(first_list, 3, 4)
+print "THe numbers are " + str(numbers(first_list, 3, 4))
 
 # should return 3, 3 , 4
 
-print time.clock() - begin_time
+print "The program execution time is " + str(time.clock() - begin_time)
