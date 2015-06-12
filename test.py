7a35c08d0ca8d29866d8dedb5859d9c774ebf9b0
Jubril Issa <jubrilissa@gmail.com>
2015-06-12 08:27:08 +0100

    Add the main test file

---
+++ test.py
@@ -0,0 +1,37 @@
+import time
+
+begin_time = time.clock()
+
+
+def numbers(lsnum, numN, numM):
+	for s in lsnum:
+		myList = []
+    	for i in range(1, n+1):
+        	if n % i == 0:
+            	myList.append(i)
+        return mylist
+
+	result = []
+	# result > numN
+	return result
+
+
+# numbers(2, 3, 4)
+
+def commonfactors(n):
+    myList = []
+    for i in range(1, n+1):
+        if n % i == 0:
+            myList.append(i)
+
+    return myList
+
+
+first_list = [9, 4, 36, 1]
+
+
+print numbers(first_list, 3, 4)
+
+# should return 3, 3 , 4
+
+print time.clock() - begin_time
